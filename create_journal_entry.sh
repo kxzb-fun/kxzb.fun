574e8b32a860009d303233390e5bd4fde44c9b40
kxzb.fun <sunday_ever@163.com>
2024-05-25 20:18:45 +0800

    feat: add journal

--- create_journal_entry.sh
+++ create_journal_entry.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-hugo new journal/$(date -u +"%Y/%m/%d").zh.md --force # 强制覆盖
+if [ $# -eq 1 ]; then
+    DATE_FORMAT=$(date -u +"%Y/%m/$1")
+else
+    DATE_FORMAT=$(date -u +"%Y/%m/%d")
+fi
+
+hugo new journal/"$DATE_FORMAT".zh.md --force # 强制覆盖
